fa97f670bb64c2fa0118f04d75b0cd706b8a8a18
SivashankaranK <sivashankaran.K1999@gmail.com>
2023-10-23 21:42:14 +0530

    Task-3 - Implementing data visualization for user

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,32 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: Number,
+  price_def: Number,
+  ratio: Number,
+  upper_bound: Number,
+  lower_bound: Number,
   timestamp: Date,
+  trigger_alert: Number | undefined
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(resposne: ServerRespond[]): Row {
+    const abcPrice = (resposne[0].top_ask.price + resposne[0].top_bid.price) / 2;
+    const defPrice = (resposne[1].top_ask.price + resposne[1].top_bid.price) / 2;
+    const ratio = abcPrice / defPrice;
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
+    return {
+      price_abc: abcPrice,
+      price_def: defPrice,
+      ratio,
+      timestamp: resposne[0].timestamp > resposne[1].timestamp ?
+        resposne[0].timestamp : resposne[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
   }
 }
